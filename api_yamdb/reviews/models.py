133231dbe34f215b06b24bec9257ad3ecd56fb61
Fedorenko Ilia <fedorenkoilia@gmail.com>
2025-02-19 19:40:19 +0100

    наисал модели для коментариев и отзывов

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,1 +1,53 @@
+from django.db.models import Avg
+from django.contrib.auth import get_user_model
+from django.db import models
 
+User = get_user_model()
+
+
+class Review(models.Model):
+    title = models.ForeignKey(Title, on_delete=models.CASCADE, related_name='reviews')
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        related_name='reviews',
+        on_delete=models.CASCADE
+    )
+    score = models.PositiveSmallIntegerField(
+        verbose_name='Оценка', choices=[(r, r) for r in range(1, 11)],
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата оценки', auto_now_add=True, db_index=True
+    )
+
+    def __str__(self):
+        return self.text
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.score_avg = Review.objects.filter(title_id=self.title).aggregate(
+            Avg('score')
+        )
+        self.title.rating = self.score_avg['score__avg']
+        self.title.save()
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True
+    )
+
+    def __str__(self):
+        return self.text
