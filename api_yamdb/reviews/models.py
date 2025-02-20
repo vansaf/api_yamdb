ce3b169551c29ae5b76b00090f95cc856a1792c5
Иван Сафонов <applesafonov@gmail.com>
2025-02-20 17:28:46 +0500

    Add model User

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    username = models.CharField('Логин', max_length=150,
+                                unique=True, null=False)
+    email = models.EmailField('Электронная почта',
+                              max_length=254, unique=True, null=False)
+    first_name = models.TextField('Имя', max_length=150, blank=True)
+    last_name = models.TextField('Фамилия', max_length=150, blank=True)
+    bio = models.TextField('Биография', blank=True)
+    role = models.CharField('Статус', default='user')
+
+    def __str__(self):
+        return self.username
