ad76136a4e3b3827ea060b74e45f893c92ecfb65
Serykh Kirill <s.kirill123@mail.ru>
2025-02-20 16:42:30 +0700

    Создан views в приложении api

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+"""
+Здесь описываются классы ViewSet,
+которые определяют логику обработки запросов
+к моделям Category, Genre и Title.
+"""
 
-# Create your views here.
+# DRF-класс для создания набора представлений
+from rest_framework import viewsets
+# Импортируем наши модели
+from reviews.models import Category, Genre, Title
+# Импорт сериализаторов
+from .serializers import CategorySerializer, GenreSerializer, TitleSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet для работы с категориями (Category).
+    Наследуется от ModelViewSet,
+    который предоставляет стандартные действия:
+    list(), create(), retrieve(), update(), partial_update(), destroy().
+    """
+    queryset = Category.objects.all()  # Указываем, какие данные обрабатываем
+    serializer_class = CategorySerializer  # Какой сериализатор использовать
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet для работы с жанрами (Genre).
+    Аналогично CategoryViewSet.
+    """
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet для работы с произведениями (Title).
+    """
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
