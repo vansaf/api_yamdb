f8f9e51c39c058b4bf2f1cd0d70a3eaa2c6ef24f
Serykh Kirill <s.kirill123@mail.ru>
2025-02-20 14:04:06 +0700

    Добавлена функция include. Добавлено описание путей

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,3 +1,8 @@
+"""
+Главный файл маршрутизации (urls) проекта YaMDb.
+Здесь указываются пути (path), по которым мы можем получить
+доступ к функционалу всего приложения.
+"""
 """YaMDb URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -14,11 +19,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# Функции для описания маршрутов
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    # URL админки Django: стандартный путь,
+    # даёт доступ к встроенной админ-панели Django.
     path('admin/', admin.site.urls),
+
+    # Подключаем все URL из приложения "api" под префиксом "api/".
+    # То есть, всё, что начинается с "/api/" в браузере,
+    # будет искать маршруты в файле api/urls.py
+    path('api/', include('api.urls')),
+
+    # Подключаем страницу redoc,
+    # это документация в формате Redoc,
+    # которая обычно хранится в шаблоне redoc.html
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
