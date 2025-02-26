c6ff04c9b79008a9d0a16fa60b0fdaddd4b98e31
Иван Сафонов <applesafonov@gmail.com>
2025-02-26 17:58:28 +0500

    Add constants/fix model

--- api_yamdb/api/utils.py
+++ api_yamdb/api/utils.py
@@ -1,14 +1,16 @@
 from random import choice
 from django.core.mail import send_mail
 
+from .constants import SUBJECT, FROM_EMAIL
+
 
 def generate_confirmation_code():
     return ''.join(choice('0123456789') for _ in range(6))
 
 
 def send_confirmation_code(email, confirmation_code):
-    subject = 'Подтверждение регистрации'
+    subject = SUBJECT
     message = f'Код подтверждения: {confirmation_code}'
-    from_email = 'support_yamdb@yandex.ru'
-    recipient_list = (email, )
+    from_email = FROM_EMAIL
+    recipient_list = (email,)
     send_mail(subject, message, from_email, recipient_list)
